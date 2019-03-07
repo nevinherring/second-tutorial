d269edbab1ebd39314696269fa13b20b14d044e6
Nevin Herring <nevin.herring@gmail.com>
2019-03-06 23:17:03 -0600

    added components folder and bannerjs

---
+++ src/components/Header/Banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { name } from '../../data';
+
+// export default function Banner() {
+//     return (
+//         <div>
+//             <h1>Hello From Banner {name}</h1>
+//         </div>
+//     )
+// }
+
+function Banner() {
+    return (
+        <div>
+            <h1>Hellp From Banner {name}</h1>
+        </div>
+    )
+}
+
+export default Banner;
