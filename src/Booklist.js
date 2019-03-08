67abdad4c5e6f0c6c185da61a4bbb316be2a1260
Nevin Herring <nevin.herring@gmail.com>
2019-03-07 23:06:20 -0600

    iterating through array to display all fake books

--- src/Booklist.js
+++ src/Booklist.js
@@ -4,23 +4,33 @@ import React, { Component } from 'react';
 import Book from "./Book";
 
 export default class Booklist extends Component {
-    books = [
-        {
-            book: "book number one",
-            author: "john doe"
-        },
-        {
-            book: "book number two",
-            author: "bobby doe"
-        }
-    ];
+    state = {
+        books: [
+            {
+                id: 1,
+                book: "book number one",
+                author: "john doe"
+            },
+            {
+                id: 2,
+                book: "book number two",
+                author: "bobby doe"
+            },
+            {
+                id: 3,
+                book: "book number three",
+                author: "peter doe"
+            }
+        ]
+    }
 
+    // very important this.setState({})
     render() {
+        //const books = this.state.books.map((item) => item.book);
         return (
             <section>
                 <h3>This is our Booklist</h3>
-                <Book book={this.books[0]} />
-                <Book book={this.books[1]} />
+                {this.state.books.map((item) => <Book key: {item.id} info={item} />)}
             </section>
         )
     }
